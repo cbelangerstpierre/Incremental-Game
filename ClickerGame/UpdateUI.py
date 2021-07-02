0a18ddb088d3a1bf13cd1ce0859306df9c5d96a2
cbelangerstpierre <cedricbstpierre@yahoo.com>
2021-07-01 23:58:00 -0400

    Added functions allover

--- ClickerGame/UpdateUI.py
+++ ClickerGame/UpdateUI.py
@@ -12,51 +12,67 @@ class UpdateUI:
         if float(number) >= 10000:
             number = "{:.2f}".format(float(number))
             number = f"{number[0]}.{number[1]}E{len(number) - 4}"
+
         return number
 
     def updateFollowers(self):
         self.ui.num_of_followers_label.setText(f"{self.scientificNumber(self.followers.getFollowers())} followers")
 
-    def updateNumOfBuysCost(self):
-        if self.ui.num_of_buys_number != 3:
-            for building in self.buildings.building_list:
-                # function
-                new_cost_shown = building.getCost()
-                for i in range(int(self.ui.num_of_buys_list[self.ui.num_of_buys_number]) - 1):
-                    new_cost_shown += building.getCost() * building.getCostMultiplier() ** (i + 1)
-                new_cost_shown = "{:.2f}".format(new_cost_shown)
-                building.setCostShown(new_cost_shown)
-                # end function
+    def setCostShownLessThanMax(self, building):
+        new_cost_shown = building.getCost()
+        for i in range(int(self.ui.num_of_buys_list[self.ui.num_of_buys_number]) - 1):
+            new_cost_shown += building.getCost() * building.getCostMultiplier() ** (i + 1)
+        new_cost_shown = "{:.2f}".format(new_cost_shown)
+        building.setCostShown(new_cost_shown)
 
-        else:
-            i = 0
-            for building in self.buildings.building_list:
-                # function
-                if float(self.followers.followers) < building.getCost():
-                    # function
-                    new_cost_shown = 0
-                    last_cost = 0
-                    able_to_buy = 1
-                    # end function
-                else:
-                    # function
-                    new_cost_shown = building.getCost()
-                    last_cost = building.getCost()
-                    able_to_buy = 1
-                    while new_cost_shown <= float(self.followers.followers):
-                        new_cost_shown += last_cost * building.getCostMultiplier()
-                        last_cost *= building.getCostMultiplier()
-                        able_to_buy += 1
-                    # end function
-                building.setCostShown("{:.2f}".format(new_cost_shown - last_cost))
-                self.ui.num_of_buys_label_list[i].setText(f"Buy x{able_to_buy - 1}")
-                i += 1
-                # end function
+    def cannotBuyOneLevel(self, building):
+        return float(self.followers.followers) < building.getCost()
 
-        i = 0
+    def setCostsAbleToBuyFromBuilding(self, building):
+        new_cost_shown = building.getCost()
+        last_cost = building.getCost()
+        able_to_buy = 1
+        while new_cost_shown <= float(self.followers.followers):
+            new_cost_shown += last_cost * building.getCostMultiplier()
+            last_cost *= building.getCostMultiplier()
+            able_to_buy += 1
+
+        return {"new_cost_shown": new_cost_shown, "last_cost": last_cost, "able_to_buy": able_to_buy}
+
+    @staticmethod
+    def initCostsAbleToBuy():
+        return {"new_cost_shown": 0, "last_cost": 0, "able_to_buy": 1}
+
+    def setLabelAbleToBuy(self, building, variables, num_of_buys_label_list_index):
+        building.setCostShown("{:.2f}".format(variables["new_cost_shown"] - variables["last_cost"]))
+        self.ui.num_of_buys_label_list[num_of_buys_label_list_index].setText(f"Buy x{variables['able_to_buy'] - 1}")
+        num_of_buys_label_list_index += 1
+
+        return num_of_buys_label_list_index
+
+    def setLabelCostAbleToBuy(self):
+        building_list_index = 0
         for cost_label in self.ui.cost_label_list:
-            cost_label.setText(f"Cost : {self.scientificNumber(self.buildings.building_list[i].cost_shown)}")
-            i += 1
+            cost_label.setText(f"Cost : "
+                               f"{self.scientificNumber(self.buildings.building_list[building_list_index].cost_shown)}")
+            building_list_index += 1
+
+    def updateNumOfBuysCost(self):
+        num_of_buys_label_list_index = 0
+        for building in self.buildings.building_list:
+            if self.ui.num_of_buys_number != 3:
+                self.setCostShownLessThanMax(building)
+
+            else:
+                if self.cannotBuyOneLevel(building):
+                    variables = self.initCostsAbleToBuy()
+
+                else:
+                    variables = self.setCostsAbleToBuyFromBuilding(building)
+
+                num_of_buys_label_list_index = self.setLabelAbleToBuy(building, variables, num_of_buys_label_list_index)
+
+        self.setLabelCostAbleToBuy()
 
     def OrangeBuildingsBackground(self):
         i = 0
